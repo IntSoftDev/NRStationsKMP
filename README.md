b55cc2c5e692b10f079d01992a360f10673fc4cb
Awais Zaka <az@intsoftdev.com>
2022-09-17 15:55:48 +0100

    copy from reference project

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# NRStations #
 
-A WIP Kotlin mulitplatform library for UK stations data.
-
-NRStations connects to a web proxy service to retrieve the locations of all the train stations in the UK.
-
-Changes are kept up-to-date when new stations open.
+# NRStations SDK
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
     dependencies {
         val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs")
-                as org.gradle.accessors.dm.LibrariesForLibs
+            as org.gradle.accessors.dm.LibrariesForLibs
         classpath(libs.bundles.gradlePlugins)
         classpath(kotlin("gradle-plugin", libs.versions.kotlin.get()))
         // NOTE: Do not place your application dependencies here; they belong
