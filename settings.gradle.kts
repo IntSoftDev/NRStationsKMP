6722a96743b8c6bdee806a69d1918ee88f8cbf6a
Awais Zaka <az@intsoftdev.com>
2022-11-27 03:42:20 +0000

    update to 0.0.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("isdlibs") {
-            from("com.intsoftdev:isdversioncatalog:0.0.2-SNAPSHOT")
+            from("com.intsoftdev:isdversioncatalog:0.0.2")
         }
     }
 }
