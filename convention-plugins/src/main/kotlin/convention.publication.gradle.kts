a51994313e307c4ed146fd5ee1fea43535f966d5
Awais Zaka <az@intsoftdev.com>
2024-04-01 00:57:27 +0100

    update to Alpha2

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -47,7 +47,7 @@ fun getExtraString(name: String) = ext[name]?.toString()
 group = "com.intsoftdev"
 // the version generated can be either release or snapshot
 // current version is 0.0.1-SNAPSHOT (update this)
-version = "1.0.0-ALPHA-1"
+version = "1.0.0-ALPHA-2"
 
 publishing {
     // Configure maven central repository
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("isdlibs") {
-            from("com.intsoftdev:isdversioncatalog:1.0.0-ALPHA-1")
+            from("com.intsoftdev:isdversioncatalog:1.0.0-ALPHA-2")
         }
     }
 }
